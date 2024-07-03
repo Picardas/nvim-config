42d573003176062f56b1568222ed9ec2a044e743
Picardas <167648023+Picardas@users.noreply.github.com>
2024-07-03 20:36:00 +0100

    Update config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -237,11 +237,48 @@ return {
             }
         },
         dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
-        opts = {}
+        opts = {
+            sources = {
+                "filesystem",
+                "buffers",
+                "git_status"
+            },
+            open_files_do_not_replace_types = {
+                "toggleterm",
+                "terminal",
+                "Trouble",
+                "trouble",
+                "qf",
+                "Outline"
+            },
+            filesystem = {
+                bind_to_cwd = false,
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+            },
+            window = {
+                mappings = {
+                    ["l"] = "open",
+                    ["h"] = "close_node",
+                    ["<space>"] = "none",
+                    ["Y"] = {
+                        function(state)
+                            local node = state.tree:get_node()
+                            local path = node:get_id()
+                            vim.fn.setreg("+", path, "c")
+                        end,
+                        desc = "Copy Path to Clipboard"
+                    },
+                    ["P"] = { "toggle_preview", config = { use_float = false }}
+                },
+                position = "right",
+                width = 26,
+            }
+        }
     }
 }
