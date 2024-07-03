1ce7dfd3d3a3c8172953b26997b48e2a9ca96837
Picardas <167648023+Picardas@users.noreply.github.com>
2024-07-03 22:34:15 +0100

    Completed lab03

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -89,7 +89,7 @@ vim.opt.confirm = true
 -- If Windows set pwsh as shell with additional configuration
 if vim.fn.has("win32") == 1 then
     vim.opt.shell = "pwsh"
-    vim.opt.shellcmdflag = "-NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
     vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
     vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
     vim.opt.shellquote = ""
